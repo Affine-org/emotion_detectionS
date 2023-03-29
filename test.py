2d68ed7d959ca9d6254a65d61beab811c0d35119
DESKTOP-PEIP6Q2\masak <masaki.ymda@gmail.com>
2023-03-29 19:14:46 +0900

    fix

--- test.py
+++ test.py
@@ -45,8 +45,8 @@ if img_file_buffer:
 
         model_pred = model.predict(np.array([face.reshape((48,48,1))]))
         predictions = model_pred.argmax()
-        st.write(np.array(model_pred))
-        st.write(predictions)
+        st.write(np.array(model_pred)[predictions])
+        # st.write(predictions)
 
         state = labels[predictions]
         font = cv2.FONT_HERSHEY_SIMPLEX
