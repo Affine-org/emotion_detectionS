3f4dd207641474de469e443da5abf0e447facdf6
DESKTOP-PEIP6Q2\masak <masaki.ymda@gmail.com>
2023-03-29 19:12:40 +0900

    fix

--- test.py
+++ test.py
@@ -45,7 +45,7 @@ if img_file_buffer:
 
         model_pred = model.predict(np.array([face.reshape((48,48,1))]))
         predictions = model_pred.argmax()
-        st.write(list(model_pred))
+        st.write(model_pred)
         st.write(predictions)
 
         state = labels[predictions]
