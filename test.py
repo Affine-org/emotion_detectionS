613b0ca8aeff353473df39a552a71729753ebd7b
DESKTOP-PEIP6Q2\masak <masaki.ymda@gmail.com>
2023-03-29 17:57:43 +0900

    fix

--- test.py
+++ test.py
@@ -28,22 +28,22 @@ if img_file_buffer:
     img_file_buffer_2 = Image.open(img_file_buffer)
     img = np.array(img_file_buffer_2)
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-detected = face_detection.detectMultiScale(gray, **settings)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    detected = face_detection.detectMultiScale(gray, **settings)
 
-for x, y, w, h in detected:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (245, 135, 66), 2)
-    cv2.rectangle(img, (x, y), (x+w//3, y+20), (245, 135, 66), -1)
-    face = gray[y+5:y+h-5, x+20:x+w-20]
-    face = cv2.resize(face, (48,48))
-    face = face/255.0
+    for x, y, w, h in detected:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (245, 135, 66), 2)
+        cv2.rectangle(img, (x, y), (x+w//3, y+20), (245, 135, 66), -1)
+        face = gray[y+5:y+h-5, x+20:x+w-20]
+        face = cv2.resize(face, (48,48))
+        face = face/255.0
 
-    predictions = model.predict(np.array([face.reshape((48,48,1))])).argmax()
-    state = labels[predictions]
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(img,state,(x+10,y+15), font, 0.5, (255,255,255), 2, cv2.LINE_AA)
-    st.markdown("#### あなたの表情は")
-    st.markdown("### {}です".format(state))
+        predictions = model.predict(np.array([face.reshape((48,48,1))])).argmax()
+        state = labels[predictions]
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img,state,(x+10,y+15), font, 0.5, (255,255,255), 2, cv2.LINE_AA)
+        st.markdown("#### あなたの表情は")
+        st.markdown("### {}です".format(state))
 
 # cv2.imshow('Facial Expression', img)
 
